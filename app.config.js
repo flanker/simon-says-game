3fa9bbcd2c950e0a4154b608f551cfa8fbc1bcba
FENG Zhichao <flankerfc@gmail.com>
2025-11-24 08:52:38 +0800

    update app config for App Store submission

---
+++ app.config.js
@@ -0,0 +1,70 @@
+function getCurrentBuildNumber() {
+  const now = new Date();
+  const pad = (n) => n.toString().padStart(2, "0");
+  return [
+    now.getFullYear(),
+    pad(now.getMonth() + 1),
+    pad(now.getDate()),
+    pad(now.getHours()),
+    pad(now.getMinutes()),
+    pad(now.getSeconds()),
+  ].join("");
+}
+
+const config = {
+  expo: {
+    name: "simon-says-game",
+    slug: "simon-says-game",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "simonsaysgame",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "me.fengzhichao.simonsays",
+      buildNumber: getCurrentBuildNumber(),
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false,
+        NSMicrophoneUsageDescription: "This app needs access to microphone to provide services.",
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        backgroundColor: "#E6F4FE",
+        foregroundImage: "./assets/images/android-icon-foreground.png",
+        backgroundImage: "./assets/images/android-icon-background.png",
+        monochromeImage: "./assets/images/android-icon-monochrome.png",
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+    },
+    web: {
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+          dark: {
+            backgroundColor: "#000000",
+          },
+        },
+      ],
+      "react-native-audio-api",
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
+    },
+  },
+};
+
+module.exports = config;
